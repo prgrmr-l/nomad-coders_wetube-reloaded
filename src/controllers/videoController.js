d9d093ed85e21480f9fdf079d29655c96cd58419
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-09-14 22:48:13 +0900

    #6.0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,7 +14,7 @@ export const trending = (req, res) => {
       commnets: 2,
       createdAt: "2 minutes ago",
       views: 59,
-      id: 1,
+      id: 2,
     },
     {
       title: "Third Video",
@@ -22,12 +22,17 @@ export const trending = (req, res) => {
       commnets: 2,
       createdAt: "2 minutes ago",
       views: 59,
-      id: 1,
+      id: 3,
     },
   ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params; // const id = req.params.id와 같은 의미
+  const video = video[id - 1];
+  console.log("Show vide".id);
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
