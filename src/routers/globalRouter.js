730db6d1357fc630024b3d4a1d5c0fa059234123
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-09-15 23:33:42 +0900

    6.2

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -7,6 +7,5 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/Jion", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
